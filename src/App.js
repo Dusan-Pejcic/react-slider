ac63d9e96b6cbe29e673d8b8a72333e7d51d81fb
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-22 12:44:11 +0200

    single review moved into a separate component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+import Review from './Review';
 
 function App() {
   const [people, setPeople] = useState(data);
@@ -34,26 +35,9 @@ function App() {
       </div>
         <div className="section-center">
           { people.map((person, personIndex)=> {
-            const {id, image, name, title, quote} = person;
-            // to add a class to the article that will determine the position and the order of the slides
-            let position = 'nextSlide';
-            if (personIndex === index) {
-              position = 'activeSlide';
-            }
-            if (personIndex === index -1 || (index === 0 && personIndex === people.length -1)) {
-              position = 'lastSlide';
-            }
-            return (
-              <article className={position} key={id}>
-                <img src={image} alt={name} className='person-img'/>
-                <h4>{name}</h4>
-                <p className="title">{title}</p>
-                <p className="text">{quote}</p>
-                <FaQuoteRight className='icon'/>
-              </article>
-            )
-              
-          })}
+            
+            return  <Review key={person.id} index={index} people={people} person={person} personIndex={personIndex}/>
+          })};
         
         <button className="prev" onClick={()=> setIndex(index -1)}>
           <FiChevronLeft />
